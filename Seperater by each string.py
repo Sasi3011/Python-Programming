3c1b474bf1ef07ae34b152f59191a092d208a8d5
Sasi <sasikiran.tt3011@gmail.com>
2025-02-18 09:18:36 +0530

    Alter for seperator

--- Seperater by each string.py
+++ Seperater by each string.py
@@ -1,8 +1,11 @@
-a = input("Enter a string: ")
-result = ""
-for i in a:
-    if i == " ":
-        result += "**"
-    else:
-        result += i
-print(result)
+# a = input("Enter a string: ")
+# result = ""
+# for i in a:
+#     if i == " ":
+#         result += "**"
+#     else:
+#         result += i
+# print(result)
+
+a=input("Enter a string: ")
+print(a.replace(" ","**"))
