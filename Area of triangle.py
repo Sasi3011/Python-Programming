b461e119f50c5a40c6ecd14a5d8e1aae018dd843
Sasi <sasikiran.tt3011@gmail.com>
2025-02-18 09:17:35 +0530

    changes in area of triangle

--- Area of triangle.py
+++ Area of triangle.py
@@ -1,5 +1,4 @@
 a=(int(input("Enter the first side of the triangle: ")))
 b=(int(input("Enter the second side of the triangle: ")))
-c=(int(input("Enter the third side of the triangle: ")))
 area=0.5*a*b
 print("Area of the triangle is",area)
