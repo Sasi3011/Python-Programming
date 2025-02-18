5138a78d35f01677179eaabaca2aefebb75ecd11
Sasi <sasikiran.tt3011@gmail.com>
2025-02-18 08:58:24 +0530

    Added

--- Sample.py
+++ Sample.py
@@ -1,12 +1,16 @@
-"Sample program"
-print("Hello, World!")
-print("Goodbye, World!")
-print("Hello, World!")
+# "Sample program"
+# print("Hello, World!")
+# print("Goodbye, World!")
+# print("Hello, World!")
 
-a=10
-print(a)
+# a=10
+# print(a)
 
-"""Hi
-Hello"""
-#hi
-'''Hello'''
+# """Hi
+# Hello"""
+# #hi
+# '''Hello'''
+
+a=int(input("Enter a number: "))
+b=float(input("Enter a number: "))
+print(a+b)
