ee10a44003a86d0307ccdcfa1f186c1207fc7907
Sasi <sasikiran.tt3011@gmail.com>
2025-02-18 10:05:18 +0530

    5 float numbers as an input

---
+++ 5 float numbers as an input .py
@@ -0,0 +1,6 @@
+print("Please enter 5 float numbers:")
+float_numbers = []
+for i in range(5):
+    num = float(input(f"Enter float number {i+1}: "))
+    float_numbers.append(num)
+print("You have entered:", float_numbers)
