3208639b9b410be834f71aeaddb3dca1d4e91b10
Sasi <sasikiran.tt3011@gmail.com>
2025-02-24 15:48:46 +0530

    32

---
+++ 32.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0 and number > 10:
+    print("The number is both even and greater than 10.")
+else:
+    print("The number is not both even and greater than 10.")
