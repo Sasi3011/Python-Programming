301a9541121e6a8fc482601631565bd698264445
Sasi <sasikiran.tt3011@gmail.com>
2025-02-24 15:06:31 +0530

    Bitwise Operator

---
+++ Bitwise Operator.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the number :"))
+b=int(input("Enter the number :"))
+print("Bitwise AND            :",a&b)
+print("Bitwise OR             :",a|b)
+print("Bitwise XOR            :",a^b)
