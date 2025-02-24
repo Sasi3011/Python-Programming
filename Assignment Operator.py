a4f0f4210c756aae01fc75b7f70187db534ed8c4
Sasi <sasikiran.tt3011@gmail.com>
2025-02-24 14:19:38 +0530

    Assignment Operator

---
+++ Assignment Operator.py
@@ -0,0 +1,27 @@
+a=int(input("Enter the number :"))
+b=int(input("Enter the number :"))
+print("Addition               :",a)
+a+=b
+print("Addition               :",a)
+print("Subtraction            :",a)
+a-=b
+print("Subtraction            :",a)
+print("Multiplication         :",a)
+a*=b
+print("Multiplication         :",a)
+print("Division               :",a)
+a/=b
+print("Division               :",a)
+print("Modulus                :",a)
+a%=b
+print("Modulus                :",a)
+print("Exponent               :",a)
+a**=b
+print("Exponent               :",a)
+print("Floor division         :",a)
+a//=b
+print("Floor division         :",a)
+a += 1
+print("Increment              :",a)
+a -= 1
+print("Decrement              :",a)
