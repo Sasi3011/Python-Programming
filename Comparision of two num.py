c3cb45ee0f991108f587e0b564c40dfb8bb0c070
Sasi <sasikiran.tt3011@gmail.com>
2025-02-24 15:22:01 +0530

    Comparision of two num

---
+++ Comparision of two num.py
@@ -0,0 +1,26 @@
+a=int(input("Enter the number :"))
+b=int(input("Enter the number :"))
+if a==b:
+    print("Equal")
+else:
+    print("Not Equal")
+if a!=b:
+    print("Not Equal")
+else:
+    print("Equal")
+if a>b:
+    print("Greater than")
+else:
+    print("Less than")
+if a<b:
+    print("Less than")
+else:
+    print("Greater than")
+if a>=b:
+    print("Greater than or equal")
+else:
+    print("Less than or equal")
+if a<=b:
+    print("Less than or equal")
+else:
+    print("Greater than or equal")
