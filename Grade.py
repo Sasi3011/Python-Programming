6b2e51a75c35346db2d455996f6ef3b786c22c29
Sasi <sasikiran.tt3011@gmail.com>
2025-02-27 09:42:59 +0530

    Grade

---
+++ Grade.py
@@ -0,0 +1,13 @@
+a=(int(input("Enter a number: ")))  
+if a>90:
+    print("Grade A")
+elif a>80:
+    print("Grade B")
+elif a>70:
+    print("Grade C")
+elif a>60:
+    print("Grade D")
+elif a>50:
+    print("Grade E")
+else:
+    print("Fail")
