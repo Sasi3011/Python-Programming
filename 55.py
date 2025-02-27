741ada4d24490de0ef4f75c8722218a77e758306
Sasi <sasikiran.tt3011@gmail.com>
2025-02-27 07:21:45 +0530

    55

---
+++ 55.py
@@ -0,0 +1,11 @@
+mark=int(input("Enter mark"))
+if(mark>=90):
+    print("A")
+elif(mark>=80 and mark<=89):
+    print("B")
+elif(mark>=70 and mark<=79):
+    print("C")
+elif(mark>=60 and mark<=69):
+    print("D")
+else:
+    print("Fail")
