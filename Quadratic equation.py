26c807a68468236b98303579cefb28d658cf8310
Sasi <sasikiran.tt3011@gmail.com>
2025-03-04 21:54:13 +0530

    Modified Quadratic equation

--- Quadratic equation.py
+++ Quadratic equation.py
@@ -1,19 +1,14 @@
-a=int(input("Enter the value of a: "))
-b=int(input("Enter the value of b: "))
-c=int(input("Enter the value of c: "))
-d=(b**2)-(4*a*c)
-if d>0:
-    x1=(-b+(d**0.5))/(2*a)
-    x2=(-b-(d**0.5))/(2*a)
+import cmath
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+c = int(input("Enter the value of c: "))
+d = (b**2) - (4*a*c)
+root1 = (-b + cmath.sqrt(d)) / (2*a)
+root2 = (-b - cmath.sqrt(d)) / (2*a)
+if d > 0:
     print("The roots are real and distinct.")
-    print("The roots are",x1,"and",x2)
-elif d==0:
-    x1=-b/(2*a)
+elif d == 0:
     print("The roots are real and equal.")
-    print("The root is",x1)
 else:
     print("The roots are imaginary.")
-    x1=-b/(2*a)
-    x2=(abs(d)**0.5)/(2*a)
-    print("The roots are",x1,"+ i",x2,"and",x1,"- i",x2)
-    
+print("The roots are", root1, "and", root2) 
