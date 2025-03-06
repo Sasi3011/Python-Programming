344aa4cb72cd0c2530160937994abe7670851e28
Sasi <sasikiran.tt3011@gmail.com>
2025-03-06 18:46:10 +0530

    52

--- 52.py
+++ 52.py
@@ -6,4 +6,4 @@ elif (units <= 300):
     bill = (100 * 5) + ((units - 100) * 7)
 else:
     bill = (100 * 5) + (200 * 7) + ((units - 300) * 10)
-print("Electricity Bill:", bill)
+    print("Electricity Bill:", bill)
