b8e203e14e50cab4a54a37509c61ef41a1b8dcd2
Sasi <sasikiran.tt3011@gmail.com>
2025-04-01 14:04:39 +0530

    Dict

--- Dictionary.py
+++ Dictionary.py
@@ -12,8 +12,20 @@
 # print(vowels)
 
 
-x=dict(name="Zophie", species="cat", age=7)
-y=x.copy()
+# x=dict(name="Zophie", species="cat", age=7)
+# y=x.copy()
+# print(y)
+# print(id(x))
+# print(id(y))
+
+tv={'size': 55, 'price': 1200}
+keymax=min(tv,key=tv.get)
+print(keymax)
+
+tv={'size': 55, 'price': 1200}
+keymax=max(tv,key=tv.get)
+print(keymax)
+
+dict1={'Name':547, 'Species':45, 'age':7}
+y=sorted(dict1.items(),key=lambda x:x[1],reverse=True)
 print(y)
-print(id(x))
-print(id(y))
