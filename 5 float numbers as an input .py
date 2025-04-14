d7a651e4a377542b19787785a28a28f2f1de6d69
Sasi <sasikiran.tt3011@gmail.com>
2025-04-14 09:55:04 +0530

    Deleted

--- 5 float numbers as an input .py
+++
@@ -1,6 +0,0 @@
-print("Please enter 5 float numbers:")
-float_numbers = []
-for i in range(5):
-    num = float(input(f"Enter float number {i+1}: "))
-    float_numbers.append(num)
-print("You have entered:", float_numbers)
