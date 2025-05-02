8dfd76d1860ce6665d3e16d2ae9d6d74f0c58e9a
Sasi <sasikiran.tt3011@gmail.com>
2025-05-02 11:44:05 +0530

    Calender

--- EXPERT_SESSION/Calender.py
+++ EXPERT_SESSION/Calender.py
@@ -1,6 +1,7 @@
 import calendar
 
-y=int(input("Enter year: "))
-m=int(input("Enter month: "))
+# y=int(input("Enter year: "))
+# m=int(input("Enter month: "))
 
-print(calendar.month(y,m))
+# print(calendar.month(y,m))
+print(calendar.month(int(input("Enter year: ")),int(input("Enter month: "))))
